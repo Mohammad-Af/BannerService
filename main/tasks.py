f5fbfeae6c062b7c8f5cfddf230e554f2ba211a9
Mohammad <mohammad.m.m.af.1999@gmail.com>
2021-06-14 11:10:31 +0430

    Edit cron expresion

--- main/tasks.py
+++ main/tasks.py
@@ -8,7 +8,7 @@ from BannerService.settings import BASE_DIR
 logger = get_task_logger(__name__)
 
 
-@periodic_task(run_every=(crontab(hour='*/12')), name="data_loader", ignore_result=True)
+@periodic_task(run_every=(crontab(minute=0, hour='*/12')), name="data_loader", ignore_result=True)
 def start():
     logger.info("Getting new data from S3")
     os.system(f"source {os.path.join(BASE_DIR, 'main/celery_task.sh')}")
